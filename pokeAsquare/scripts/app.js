dc487edc6be88a1487b833760f2e1cbd0ad22ab0
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-09-21 12:04:18 -0500

    QUESTION 2

--- pokeAsquare/scripts/app.js
+++ pokeAsquare/scripts/app.js
@@ -10,10 +10,27 @@ $('button').on('click', () => {
 // that will create an arbitary number of divs depending on the parameter
 // and attach them to the squares class from the html
 const createSquares = (numberOfSquares) => {
-  for (let i = 0; i <= numberOfSquares; i++) {
+  for (let i = 0; i < numberOfSquares; i++) {
     // creating squares
     const $div = $('<div/>');
+    // This is where the color is applied
+    applyRandomColor($div);
     // attach to the square class
     $('.squares').append($div);
   }
 }
+
+// Write a function, that when we apply click it randomizes squares to
+// red, blue, or green
+const applyRandomColor = (square) => {
+  const randNum = Math.floor(Math.random() * 3) + 1;
+
+  //
+  if (randNum === 1) {
+    square.css('background-color', 'red');
+  } else if (randNum === 2) {
+    square.css('background-color', 'blue');
+  } else {
+    square.css('background-color', 'green');
+  }
+};
