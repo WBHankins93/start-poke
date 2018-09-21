691faaabc17a1989acf87b41fd77a22e1e12fd46
Ben <WBHankins93@Bens-MacBook-Air.local>
2018-09-21 11:32:41 -0500

    Click works

--- pokeAsquare/scripts/app.js
+++ pokeAsquare/scripts/app.js
@@ -1,1 +1,19 @@
+$('button').on('click', () => {
+  console.log('click works');
+  // When the user clicks the button, populate the squares
+  createSquares(30);
+});
 
+
+// Now we need to write a function named (createSquares) that sets up our
+// squares, Try to write a function that takes a parameter numberOfSquares
+// that will create an arbitary number of divs depending on the parameter
+// and attach them to the squares class from the html
+const createSquares = (numberOfSquares) => {
+  for (let i = 0; i <= numberOfSquares; i++) {
+    // creating squares
+    const $div = $('<div/>');
+    // attach to the square class
+    $('.squares').append($div);
+  }
+}
